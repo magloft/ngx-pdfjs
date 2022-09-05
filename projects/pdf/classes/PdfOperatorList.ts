e70eb8a4a272c33e86d1305a0604225ab3a27e09
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2022-09-05 17:12:47 +0800

    upgrade to 14.2.0

--- projects/pdf/classes/PdfOperatorList.ts
+++ projects/pdf/classes/PdfOperatorList.ts
@@ -1,4 +1,4 @@
-import md5 from 'blueimp-md5'
+import md5 from 'md5'
 import { OPS } from 'pdfjs-dist'
 import { PDFOperatorList } from 'pdfjs-dist/types/display/api'
 import { PdfOperator, PdfOperatorFilter } from './PdfOperator'
